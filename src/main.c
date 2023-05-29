04660322682bd53e54b7dcab92ed4f329df20e60
Jonas Senft-Raiss <jonas.senftraiss@gmail.com>
2023-05-29 23:45:29 +0100

    Added disassembler to debug Instructions in the chunk

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include "../include/common.h"
 #include "../include/chunk.h"
+#include "../include/debug.h"
 
 int main(int argc, const char* argv[]) {
   Chunk chunk;
   initChunk(&chunk);
   writeChunk(&chunk, OP_RETURN);
+
+  disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
   return 0;
 }
